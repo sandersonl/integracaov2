998d7508bcbb26fd1be8576d378ba62dca41738a
Lucas Sanderson dos Santos Passos <lucas.passos@eletra.local>
2024-12-13 11:43:06 -0300

    homologation fixes v2

--- src/main/java/com/integracao/Main.java
+++ src/main/java/com/integracao/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("MainView.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("views/MainView.fxml"));
         VBox root = loader.load();
 
         Scene scene = new Scene(root, 800, 600);
